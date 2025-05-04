8b88094f0f50c00e0a2ddc846946a870b0f5c564
Nolan Hill <nhill8@huskers.unl.edu>
2025-05-04 16:07:01 -0500

    Completed section 4 for the rotary encoder

--- src/rotary-encoder.c
+++ src/rotary-encoder.c
@@ -2,7 +2,7 @@
  *
  * @file rotary-encoder.c
  *
- * @author (STUDENTS -- TYPE YOUR NAME HERE)
+ * @author (STUDENTS -- Nolan Hill)
  * @author (STUDENTS -- TYPE YOUR NAME HERE)
  *
  * @brief Code to determine the direction that a rotary encoder is turning.
@@ -14,47 +14,105 @@
  * ComboLock solution (c) the above-named students
  */
 
-#include <CowPi.h>
-#include "interrupt_support.h"
-#include "rotary-encoder.h"
-
-#define A_WIPER_PIN         (16)
-#define B_WIPER_PIN         (A_WIPER_PIN + 1)
-
-typedef enum {
-    HIGH_HIGH, HIGH_LOW, LOW_LOW, LOW_HIGH, UNKNOWN
-} rotation_state_t;
-
-static rotation_state_t volatile state;
-static direction_t volatile direction = STATIONARY;
-static int volatile clockwise_count = 0;
-static int volatile counterclockwise_count = 0;
-
-static void handle_quadrature_interrupt();
-
-void initialize_rotary_encoder() {
-    cowpi_set_pullup_input_pins((1 << A_WIPER_PIN) | (1 << B_WIPER_PIN));
-    ;
-//    register_pin_ISR((1 << A_WIPER_PIN) | (1 << B_WIPER_PIN), handle_quadrature_interrupt);
-}
-
-uint8_t get_quadrature() {
-    ;
-    return 0;
-}
-
-char *count_rotations(char *buffer) {
-    ;
-    return buffer;
-}
-
-direction_t get_direction() {
-    ;
-    return STATIONARY;
-}
-
-static void handle_quadrature_interrupt() {
-    static rotation_state_t last_state = UNKNOWN;
-    uint8_t quadrature = get_quadrature();
-    ;
-}
+ #include <CowPi.h>
+ #include "interrupt_support.h"
+ #include "rotary-encoder.h"
+ 
+ #define A_WIPER_PIN         (16)
+ #define B_WIPER_PIN         (A_WIPER_PIN + 1)
+ 
+ typedef enum {
+     HIGH_HIGH, HIGH_LOW, LOW_LOW, LOW_HIGH, UNKNOWN
+ } rotation_state_t;
+ 
+ static rotation_state_t volatile state;
+ static direction_t volatile direction = STATIONARY;
+ static int volatile clockwise_count = 0;
+ static int volatile counterclockwise_count = 0;
+ 
+ static cowpi_ioport_t volatile *ioport;
+ 
+ static void handle_quadrature_interrupt();
+ 
+ void initialize_rotary_encoder() {
+     cowpi_set_pullup_input_pins((1 << A_WIPER_PIN) | (1 << B_WIPER_PIN));
+     uint32_t quadrature = get_quadrature(); 
+     direction = STATIONARY;
+ 
+     switch (quadrature) {
+         case 0b11: 
+             state = HIGH_HIGH; 
+             break;
+         case 0b10: 
+             state = HIGH_LOW; 
+             break;
+         case 0b00: 
+             state = LOW_LOW; 
+             break;
+         case 0b01: 
+             state = LOW_HIGH; 
+             break;
+         default:   
+             state = UNKNOWN; 
+             break;
+     }
+ 
+     register_pin_ISR((1 << A_WIPER_PIN) | (1 << B_WIPER_PIN), handle_quadrature_interrupt);
+ }
+ 
+ uint8_t get_quadrature() {
+     ioport = (cowpi_ioport_t *)(0xD0000000);
+ 
+     uint32_t quadrature = ioport->input & (0b11 << 16);
+     return (quadrature >> 16);
+ }
+ 
+ char *count_rotations(char *buffer) {
+     sprintf(buffer, "CW:%d CCW:%d", clockwise_count, counterclockwise_count);
+     return buffer;
+ }
+ 
+ direction_t get_direction() {
+     direction_t result = direction;
+     direction = STATIONARY;
+     return result;
+ }
+ 
+ static void handle_quadrature_interrupt() {
+     static rotation_state_t last_state = UNKNOWN;
+     rotation_state_t previous_state = state;
+     uint8_t quadrature = get_quadrature();
+     
+ 
+     switch (quadrature) {
+         case 0b11:
+             state = HIGH_HIGH;
+             break;
+ 
+         case 0b10:
+             state = HIGH_LOW;
+             break;
+ 
+         case 0b00:
+             if ((previous_state == HIGH_LOW && last_state == HIGH_HIGH)) {
+                 clockwise_count++;
+                 direction = CLOCKWISE;
+                 state = LOW_LOW;
+             } else if ((previous_state == LOW_HIGH && last_state == HIGH_HIGH)) {
+                 counterclockwise_count++;
+                 direction = COUNTERCLOCKWISE;
+                 state = LOW_LOW;
+             }
+             break;
+ 
+         case 0b01:
+             state = LOW_HIGH;
+             break;
+ 
+         default:
+             state = UNKNOWN;
+             break;
+     }
+ 
+     last_state = previous_state;
+ }
