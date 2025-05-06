0b2fa74a4e13c48f984e73569988beaf556f7da3
Nick Goertzen <ngoertzen2@huskers.unl.edu>
2025-05-05 21:54:07 -0500

    Almost finished section 7

--- src/lock-controller.c
+++ src/lock-controller.c
@@ -21,132 +21,218 @@
  #include "servomotor.h"
  
  
- typedef enum {
-     LOCKED, UNLOCKED, CHANGING, ALARMED
- } lock_state_t;
+typedef enum {
+    LOCKED, UNLOCKED, CHANGING, ALARMED
+} lock_state_t;
+
+static lock_state_t state = LOCKED;
+static direction_t correct_direction;
+static uint8_t combination[3] __attribute__((section (".uninitialized_ram.")));
+static int bad_tries = 0;
+static uint8_t entered_combination[3];
+static int visible_counts[3] = {0, 0, 0};
+static uint8_t current_number = 0;
+static int entry_stage = 0;
+static bool handle_right_press = false;
+static cowpi_timer_t volatile *timer;
+
+uint8_t const *get_combination() {
+return combination;
+}
  
- static lock_state_t state = LOCKED;
- static uint8_t combination[3] __attribute__((section (".uninitialized_ram.")));
- static int bad_tries = 0;
- static uint8_t entered_combination[3];
- static int visible_counts[3] = {0, 0, 0};
- static uint8_t current_number = 0;
- static int entry_index = 0;
- static int entry_stage = 0;
- char display_buffer[9];
+void force_combination_reset() {
+    combination[0] = 5;
+    combination[1] = 10;
+    combination[2] = 15;
+}
  
- uint8_t const *get_combination() {
-     return combination;
- }
+bool check_combination(void) {
+    return entered_combination[0] == combination[0]
+        && entered_combination[1] == combination[1]
+        && entered_combination[2] == combination[2]
+        && visible_counts[0] >= 3
+        && visible_counts[1] == 2
+        && visible_counts[2] == 1;
+}
+
+uint32_t get_microseconds(void) {
+    return timer->raw_lower_word;
+}
  
- void force_combination_reset() {
-     combination[0] = 5;
-     combination[1] = 10;
-     combination[2] = 15;
- }
+void blink_leds(){
+    for (int i = 0; i < 2; i++) {
+        cowpi_illuminate_left_led();
+        cowpi_illuminate_right_led();
+        uint32_t start_time = get_microseconds();
+        while ((get_microseconds() - start_time) < 250000) {}
+        cowpi_deluminate_left_led();
+        cowpi_deluminate_right_led();
+        start_time = get_microseconds();
+        while ((get_microseconds() - start_time) < 250000) {}
+
+    }
+}
+
+void clear_combination() {
+    entry_stage = 0;
+    current_number = 0;
+    correct_direction = CLOCKWISE;
+    for (int i = 0; i < 3; i++) {
+        entered_combination[i] = 0xFF;
+        visible_counts[i] = 0;
+    }
+}
+
+void display_combination() {
+    char display[32] = "";
+    for (int i = 0; i < 3; i++) {
+        char temp[5];
+        if (i == entry_stage) {
+            sprintf(temp, "%02d", current_number);
+            strcat(display, temp);
+        } else if (entered_combination[i] != 0xFF) {
+            sprintf(temp, "%02d", entered_combination[i]);
+            strcat(display, temp);       
+        } else {
+            strcat(display, "  ");
+        }
+        if (i < 2) {
+            strcat(display, "-");
+        }
+    }
+    display_string(1, display);
+}
  
- bool check_combination(void) {
-     return entered_combination[0] == combination[0]
-         && entered_combination[1] == combination[1]
-         && entered_combination[2] == combination[2]
-         && visible_counts[0] >= 3
-         && visible_counts[1] >= 2
-         && visible_counts[2] >= 1;
- }
- 
- static uint8_t one_through_fifteen(uint8_t n) {
-     return (n + 16) % 16;
- }
- 
- void update_combination_visual() { //not sure if this works how Id want it to but I havent tested it 
-     snprintf(display_buffer, sizeof(display_buffer), "%02d-%02d-%02d",
-              one_through_fifteen(entered_combination[0]),
-              one_through_fifteen(entered_combination[1]),
-              one_through_fifteen(entered_combination[2]));
-     display_string(0, display_buffer);
- }
- 
- void blink_leds(){
-     // need to make this yet
- }
- 
- // enter_number()  needs to be made
- 
- 
- void initialize_lock_controller() {
-     state = LOCKED;
-     entry_index = 0;
-     bad_tries = 0;
-     for (int i = 0; i < 3; i++) {
-         entered_combination[i] = 0xFF;
-         visible_counts[i] = 0;
-     }
-     current_number = 0;
- 
-     cowpi_illuminate_left_led();
-     cowpi_deluminate_right_led();
-     rotate_full_clockwise();
-     display_string(0, "  -  -  ");
- }
- 
- 
- void control_lock() {
-     if (state == ALARMED) {
-         blink_leds();
-         return;
-     }
- 
-     direction_t dir = get_direction();
- 
-     if(state == LOCKED){
-         if (entry_stage < 3) {
-             //enter_number()
-         } else {
-             if (dir == COUNTERCLOCKWISE) {
-                 //clear combination here
-                 display_string(0, "-- -- --");
-             }
-             if (cowpi_left_button_is_pressed()) {
-                 if (check_combination()) {
-                     state = UNLOCKED;
-                     display_string(0, "OPEN");
-                     cowpi_deluminate_left_led();
-                     cowpi_illuminate_right_led();
-                     rotate_full_counterclockwise();
-                 } else {
-                     bad_tries++;
-                     if (bad_tries >= 3) {
-                         state = ALARMED;
-                         display_string(0, "alert!");
-                     } else{
-                     char msg[11];
-                     snprintf(msg, sizeof(msg), "bad try %d", bad_tries);
-                     display_string(0, msg);
-                     // make leds blink twice here
-                     //clear combination here
-                     display_string(0, "-- -- --");
-                     }
-                 }
-             }
-         }
-     }
- 
-     else if(state == UNLOCKED){
-         if (cowpi_left_button_is_pressed() && cowpi_right_button_is_pressed()) {
-             state = LOCKED;
-             cowpi_illuminate_left_led();
-             cowpi_deluminate_right_led();
-             rotate_full_clockwise();
-             //clear combination here
-             display_string(0, "-- -- --");
-         }
-     } 
-     
-     if(state == CHANGING){
-         //needs to be able to change the combination after solving one I believe
-     }
- 
- }
+void initialize_lock_controller() {
+    state = LOCKED;
+    entry_stage = 0;
+    correct_direction = CLOCKWISE;
+    bad_tries = 0;
+    timer = (cowpi_timer_t *) (0x40054000);
+    for (int i = 0; i < 3; i++) {
+        entered_combination[i] = 0xFF;
+        visible_counts[i] = 0;
+    }
+    current_number = 0;
+    cowpi_illuminate_left_led();
+    cowpi_deluminate_right_led();
+    rotate_full_clockwise();
+    //force_combination_reset();
+    display_combination();
+}
+
+void control_lock() {
+    direction_t dir = get_direction();
+    char result[16];
+    sprintf(result, "%d %d %d", visible_counts[0], visible_counts[1], visible_counts[2]);
+    display_string(3, result);
+
+    if (state == ALARMED) {
+        while (true) {
+            blink_leds();
+        }
+    } else if (state == LOCKED) {
+        
+        if (entry_stage < 3) {
+            if (dir == correct_direction) {
+                if (current_number + 1 == combination[entry_stage]) {
+                    visible_counts[entry_stage]++;
+                }  
+                if (current_number < 15) {
+                    current_number++;
+                } else {
+                    current_number = 0;
+                } 
+            } else if (dir != correct_direction && dir != STATIONARY) {
+                entry_stage++;
+                correct_direction = (correct_direction == CLOCKWISE) ? COUNTERCLOCKWISE : CLOCKWISE;
+            }
+            entered_combination[entry_stage] = current_number;
+        } 
+        if (entry_stage > 1) {
+            if (dir == COUNTERCLOCKWISE) {
+                clear_combination();
+            }
+            if (cowpi_left_button_is_pressed()) {
+                if (check_combination()) {
+                    state = UNLOCKED;
+                } else {
+                    bad_tries++;
+                    if (bad_tries >= 3) {
+                        state = ALARMED;
+                    } else{
+                        clear_combination();
+                        blink_leds();
+                    }
+                }
+            }
+        }
+    } else if(state == UNLOCKED) {
+        if (cowpi_left_switch_is_in_right_position() && cowpi_right_button_is_pressed()) {
+            state = CHANGING;
+        }
+        if (cowpi_left_button_is_pressed() && cowpi_right_button_is_pressed()) {
+            clear_combination();
+            bad_tries = 0;
+            state = LOCKED;
+        }
+    } else if(state == CHANGING) {
+        uint8_t new_combination1[3] = {0xFF, 0xFF, 0xFF};
+        uint8_t new_combination2[3] = {0xFF, 0xFF, 0xFF};
+
+        // TODO: implement inputing new combinations from keypad.
+        if (cowpi_left_switch_is_in_left_position()) {
+            bool incomplete = false;
+            bool mismatch = false;
+            bool out_of_bounds = false;
+
+            for (int i = 0; i < 3; i++) {
+                if (new_combination1[i] == 0xFF || new_combination2[i] == 0xFF) {
+                    incomplete = true;
+                }
+                if (new_combination1[i] != new_combination2[i]) {
+                    mismatch = true;
+                }
+                if (new_combination1[i] > 15) {
+                    out_of_bounds = true;
+                }
+            }
+
+            if (incomplete || mismatch || out_of_bounds) {
+                display_string(0, "no change");
+            } else {
+                for (int i = 0; i < 3; i++) {
+                    combination[i] = new_combination1[i];
+                }
+                display_string(0, "changed");
+                state = UNLOCKED;
+                cowpi_deluminate_left_led();
+                cowpi_illuminate_right_led();
+            }
+        }
+    }
+    if (state == LOCKED) {
+        cowpi_illuminate_left_led();
+        cowpi_deluminate_right_led();
+        rotate_full_clockwise();
+        if (bad_tries > 0) {
+            char temp[15];
+            sprintf(temp, "%s%d", "bad try ", bad_tries);
+            display_string(0, temp);
+        }
+        display_combination();
+    } else if (state == UNLOCKED) {
+        cowpi_deluminate_left_led();
+        cowpi_illuminate_right_led();
+        rotate_full_counterclockwise();
+        display_string(0, "OPEN");
+    } else if (state == ALARMED) {
+        display_string(0, "alert!");
+    } else if (state == CHANGING) {
+        display_string(0, "enter");
+    }
+    
+}
  
  
  
